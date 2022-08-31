ff5e8df89fcadd0fbee64b7b66a87a0a258e7059
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2022-08-31 21:16:21 +0300

    update text on oil

--- src/views/OilChange.vue
+++ src/views/OilChange.vue
@@ -4,18 +4,72 @@
       <div class="columns">
         <div class="column box">
           <div class="content">
-            <h1>AutoBoost piedāvā</h1>
+            <h1>AutoBoost piedāvā MotulEvo priekšrocības gala patērētājiem</h1>
+
+            <h2>Jūsu priekšrocības īsumā:</h2>
             <p>
-              Oficiāls Motul Eva  atomātisko kārbu eļļas maiņas skalošanas mašīnu.
-              Skalošanas mašīna, var izmantot eļļas maiņai. Tas ir kompakts, uzticams
-              un pilnībā automātisks. 
+              Pilnīga transmisijas šķidruma nomaiņa palīdz noņemt piesārņojumu.
+              Tīrā eļļošanas ķēdē jaunais transmisijas šķidrums pilnībā sasniedz
+              savu potenciālu un nodrošina ilgstošus rezultātus.
+            </p>
+            <ol>
+              <li>
+                Vienmērīga pārnesumu pārslēgšana, uzlabots braukšanas komforts
+              </li>
+              <li>Aizsardzība pret nodilumu</li>
+              <li>Vērtību un funkciju uzturēšana</li>
+              <li>Vērtīgs ieguldījums</li>
+              <li>Degvielas ekonomijas ieguvums</li>
+            </ol>
+
+            <h2>
+              Perfekta atbilstība: transmisijas skalošana un šķidruma maiņa
+            </h2>
+
+            <h3>
+              Ir daudz praktisku iemeslu, kāpēc jums vajadzētu apvienot eļļas
+              maiņas pakalpojumu ar transmisijas skalošanu:
+            </h3>
+
+            <p>
+              Tīrā transmisijā eļļa var pilnībā attīstīt savu veiktspēju un
+              palīdz aizsargāt transmisiju pret priekšlaicīgu nodilumu un
+              bojājumiem. Optimāla eļļošana padara pārnesumu pārslēgšanu ļoti
+              efektīvu un ērtu.
+            </p>
+
+            <h3>
+              Ja pārnesumu pārslēgšanas veiktspēja ir pasliktinājusies,
+              profesionāls transmisijas serviss var atrisināt vai novērst šādas
+              problēmas:
+            </h3>
+
+            <ol>
+              <li>Aizkavēta pārnesumu pārslēgšana</li>
+              <li>Nepareiza pārnesumkārbas pārslēgšana</li>
+              <li>Transportlīdzekļa trīcēšana zem slodzes</li>
+              <li>Raustīšanās apstāšanās un stāvēšanas manevru laikā</li>
+            </ol>
+
+            <h3>Mūsu profesionālais servisa pakalpojums:</h3>
+
+            <p>
+              Jūsu dubultā sajūga pārnesumkārbas (DCT), nepārtraukti mainīgās
+              pārnesumkārbas (CVT) vai automātiskās pārnesumkārbas (AT)
+              pārnesumu pārslēgšanas darbība ir nepārtraukti pasliktinājusies
+              pēc ilgstošas ​​intensīvas lietošanas? Vai tas kratās un raustas
+              apstāšanās un stāvēšanas manevru laikā? Jūsu transportlīdzeklis
+              atsakās ieslēgt ātrumu? Pārnesumu maiņu pavada slīpēšanas
+              troksnis? Biežāk tas ir piesārņojums un nodilums, kas pasliktina
+              pārnesumu pārslēgšanas veiktspēju. Regulārs apkopes grafiks,
+              tostarp transmisijas apkope, ir galvenais, lai jūsu automašīna
+              kalpotu ilgāk.
             </p>
             <p>
-              To var izmantot jebkura veida  <strong>automātiskajām pārnesumkārbām</strong>.
-              Jaunā skalošanas iekārta ir izstrādāta, pateicoties Motul tehniķu pieredzei 
-              šajā jomā. Tas ir kompakts, uzticams un pilnībā automātisks, 
-              lai ļautu strādāt ar visa veida automātiskajām pārnesumkārbām. 
-              Tajā ir divi elektroniskie svari, eļļas filtrs ar magnētu un printeris.
+              Uzlabota pārnesumu pārslēgšana padara braukšanu vieglāku, ērtāku
+              un palielina kopējo efektivitāti. Tāpēc mēs iesakām regulāri veikt
+              transmisijas servisu, lai aizsargātu transmisiju, palielinātu tās
+              kalpošanas laiku un uzlabotu braukšanas pieredzi.
             </p>
           </div>
         </div>
